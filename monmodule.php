4a600c6148f6921ed62a60a0dc78c87faccc88f2
DanneelsSophie <danneels.s@hotmail.fr>
2017-01-16 12:40:19 +0100

    essaie d'un module

---
+++ monmodule.php
@@ -0,0 +1,22 @@
+<?php
+class MonModule extends Module
+{
+		public function __construct()
+	{
+		$this->name = 'monmodule';
+		$this->tab = 'front_office_features';
+		$this->version = '0.1';
+		$this->author = 'Danneels Sophie';
+		$this->displayName = 'Essaie module';
+		$this->description = 'A description';
+		parent::__construct();
+		
+	}
+	
+	public function getContent()
+	{
+		return 'coucou';
+	}
+	}
+
+
