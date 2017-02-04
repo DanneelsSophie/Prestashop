e76442f8fe4d6a6e73be578725247c5d3df67b91
DanneelsSophie <danneels.s@hotmail.fr>
2017-02-04 09:50:48 +0100

    Update monmodule.php

--- monmodule.php
+++ monmodule.php
@@ -15,10 +15,33 @@ class MonModule extends Module
 	
 	public function getContent()
 	{
-		
+		$this->processConfiguration();
 		return $this->display(__FILE__, 'getContent.tpl');
 
 	}
+	
+	
+	
+	}
+public function processConfiguration()
+	{
+		if(Tools::isSubmit('valid')){
+			
+			$enable_title = Tools::getValue('title');
+			$enable_text = Tools::getValue('text');
+	
+			
+			$insert = array (
+					'titre' => pSQL ($enable_title),
+					'contenu' => pSQL($enable_text),
+					'dateajout' => date('Y-m-d H:i:s'),
+					'dateupdate' => date('Y-m-d H:i:s'),
+				);
+				Db::getInstance()->insert('ps_helperlist',$insert);
+				
+		
+			$this->context->smarty->assign('confirmation','ok');
+		}
 	}
 
 
